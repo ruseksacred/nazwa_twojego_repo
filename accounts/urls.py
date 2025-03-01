970deedb72dfa306a529bd3cb1910f0d645962ea
Bartosz <brustyn123@gmail.com>
2025-03-01 23:47:19 +0100

    user-login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import user_login
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path("login/", user_login, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(reguest):
     form = CustomUserCreationForm()
@@ -13,6 +15,18 @@ def register(reguest):
             form = CustomUserCreationForm()
 
     return render(reguest,'accounts/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("admin:index")  # Zmień na nazwę swojej strony głównej
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "accounts/login.html", {"form": form})
             
 
 
